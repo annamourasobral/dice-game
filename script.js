fa1bb07b6b429720af406b9b01c9f9094317ba34
Anna Moura Sobral <annamourasobral@gmail.com>
2022-06-24 11:20:48 +0200

    display turn and dice numbers

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-// Create variables for the game state
 let player1Score = 0
 let player2Score = 0
 let player1Turn = true
 
-// Create variables to store references to the necessary DOM nodes
 const player1Dice = document.getElementById("player1Dice")
 const player2Dice = document.getElementById("player2Dice")
 const player1Scoreboard = document.getElementById("player1Scoreboard")
@@ -12,17 +10,19 @@ const message = document.getElementById("message")
 const rollBtn = document.getElementById("rollBtn")
 const resetBtn = document.getElementById("resetBtn")
 
-// Hook up a click event listener to the Roll Dice Button. 
 rollBtn.addEventListener("click", function() {
     const randomNumber = Math.floor(Math.random() * 6) + 1
     
-    // 1. Find out which players turn it is
-    // 2. log out the value e.g. "Player 1 rolled 5"
-    // 3. Switch the turn back to the other player
     if (player1Turn) {
-        console.log("player 1 rolled " + randomNumber)
+        player1Dice.textContent = randomNumber
+        player1Dice.classList.remove("active")
+        player2Dice.classList.add("active")
+        message.textContent = "Player 2 Turn"
     } else {
-       console.log("player 2 rolled " + randomNumber)
+       player2Dice.textContent = randomNumber
+       player2Dice.classList.remove("active")
+       player1Dice.classList.add("active")
+       message.textContent = "Player 1 Turn"
     }
     player1Turn = !player1Turn
 })
