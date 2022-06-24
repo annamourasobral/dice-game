3a80ac58ca7414c5484f8e67c6502ecd6aa3559c
Anna Moura Sobral <annamourasobral@gmail.com>
2022-06-24 11:07:24 +0200

    rolling dice with math random and setting rounds

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ const player2Scoreboard = document.getElementById("player2Scoreboard")
 const message = document.getElementById("message")
 const rollBtn = document.getElementById("rollBtn")
 const resetBtn = document.getElementById("resetBtn")
+
+// Hook up a click event listener to the Roll Dice Button. 
+rollBtn.addEventListener("click", function() {
+    const randomNumber = Math.floor(Math.random() * 6) + 1
+    
+    // 1. Find out which players turn it is
+    // 2. log out the value e.g. "Player 1 rolled 5"
+    // 3. Switch the turn back to the other player
+    if (player1Turn) {
+        console.log("player 1 rolled " + randomNumber)
+    } else {
+       console.log("player 2 rolled " + randomNumber)
+    }
+    player1Turn = !player1Turn
+})
