66645802bab9459b37cc0cfaad034176ad56c991
Anna Moura Sobral <annamourasobral@gmail.com>
2022-06-24 11:55:58 +0200

    reset game btn function

--- script.js
+++ script.js
@@ -28,16 +28,34 @@ rollBtn.addEventListener("click", function() {
         player1Dice.classList.add("active")
         message.textContent = "Player 1 Turn"
     }
-
-    if (player1Score >= 20) {
-        message.textContent = "Player 1 has won! 🥳"
-        rollBtn.style.display = "none"
-        resetBtn.style.display = "block"
-    } else if (player2Score >= 20) {
-        message.textContent = "Player 2 has won! 🎉"
+    
+    function showResetBtn() {
         rollBtn.style.display = "none"
         resetBtn.style.display = "block"
     }
-    
+
+    if (player1Score >= 20) {
+        message.textContent = "Player 1 has won! 🥳"
+        showResetBtn()
+    } else if (player2Score >= 20) {
+        message.textContent = "Player 2 has won! 🎉"
+        showResetBtn()
+    }
+
     player1Turn = !player1Turn
+})
+
+resetBtn.addEventListener("click", function() {
+    player1Score = 0;
+    player2Score = 0;
+    player1Turn = true;
+    message.textContent = "Player 1 Turn";
+    player2Dice.classList.remove("active");
+    player1Dice.classList.add("active");
+    player1Scoreboard.textContent = player1Score;
+    player2Scoreboard.textContent = player2Score;
+    player1Dice.textContent = "-";
+    player2Dice.textContent = "-";
+    resetBtn.style.display = "none";
+    rollBtn.style.display = "block";        
 })
